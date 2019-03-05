01eaf6f15e034f2b7e3f92c77bb4a3b06b760570
luhaikong <1031350293@qq.com>
2019-03-05 17:18:29 +0800

    适配到安卓4.0

--- safekeyboardlibrary/src/main/java/com/library/safekeyboard/SafeKeyboardView.java
+++ safekeyboardlibrary/src/main/java/com/library/safekeyboard/SafeKeyboardView.java
@@ -113,7 +113,7 @@ public class SafeKeyboardView extends KeyboardView {
                         field = KeyboardView.class.getDeclaredField(getContext().getString(R.string.mLabelTextSize));
                         field.setAccessible(true);
                         labelTextSize = (int) field.get(this);
-                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                     paint.setTextSize(labelTextSize);
@@ -124,7 +124,7 @@ public class SafeKeyboardView extends KeyboardView {
                         field = KeyboardView.class.getDeclaredField(getContext().getString(R.string.mLabelTextSize));
                         field.setAccessible(true);
                         keyTextSize = (int) field.get(this);
-                    } catch (NoSuchFieldException | IllegalAccessException e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
                     }
                     paint.setTextSize(keyTextSize);
@@ -168,6 +168,81 @@ public class SafeKeyboardView extends KeyboardView {
             e.printStackTrace();
         }
     }
+
+//    private void drawTextAndIcon(Canvas canvas, Keyboard.Key key, @Nullable Drawable drawable) {
+//        try {
+//            Rect bounds = new Rect();
+//            Paint paint = new Paint();
+//            paint.setTextAlign(Paint.Align.CENTER);
+//            paint.setAntiAlias(true);
+//            paint.setColor(Color.WHITE);
+//
+//            if (key.label != null) {
+//                String label = key.label.toString();
+//
+//                Field field;
+//
+//                if (label.length() > 1 && key.codes.length < 2) {
+//                    int labelTextSize = 0;
+//                    try {
+//                        field = KeyboardView.class.getDeclaredField(getContext().getString(R.string.mLabelTextSize));
+//                        field.setAccessible(true);
+//                        labelTextSize = (int) field.get(this);
+//                    } catch (NoSuchFieldException | IllegalAccessException e) {
+//                        e.printStackTrace();
+//                    }
+//                    paint.setTextSize(labelTextSize);
+//                    paint.setTypeface(Typeface.DEFAULT_BOLD);
+//                } else {
+//                    int keyTextSize = 0;
+//                    try {
+//                        field = KeyboardView.class.getDeclaredField(getContext().getString(R.string.mLabelTextSize));
+//                        field.setAccessible(true);
+//                        keyTextSize = (int) field.get(this);
+//                    } catch (NoSuchFieldException | IllegalAccessException e) {
+//                        e.printStackTrace();
+//                    }
+//                    paint.setTextSize(keyTextSize);
+//                    paint.setTypeface(Typeface.DEFAULT);
+//                }
+//
+//                paint.getTextBounds(key.label.toString(), 0, key.label.toString().length(), bounds);
+//                canvas.drawText(key.label.toString(), key.x + (key.width / 2),
+//                        (key.y + key.height / 2) + bounds.height() / 2, paint);
+//            }
+//            if (drawable == null) return;
+//            // 约定: 最终图标的宽度和高度都需要在按键的宽度和高度的二分之一以内
+//            // 如果: 图标的实际宽度和高度都在按键的宽度和高度的二分之一以内, 那就不需要变换, 否则就需要等比例缩小
+//            int iconSizeWidth, iconSizeHeight;
+//            key.icon = drawable;
+//            int iconH = px2dip(mContext, key.icon.getIntrinsicHeight());
+//            int iconW = px2dip(mContext, key.icon.getIntrinsicWidth());
+//            if (key.width >= (ICON2KEY * iconW) && key.height >= (ICON2KEY * iconH)) {
+//                //图标的实际宽度和高度都在按键的宽度和高度的二分之一以内, 不需要缩放, 因为图片已经够小或者按键够大
+//                setIconSize(canvas, key, iconW, iconH);
+//            } else {
+//                //图标的实际宽度和高度至少有一个不在按键的宽度或高度的二分之一以内, 需要等比例缩放, 因为此时图标的宽或者高已经超过按键的二分之一
+//                //需要把超过的那个值设置为按键的二分之一, 另一个等比例缩放
+//                //不管图标大小是多少, 都以宽度width为标准, 把图标的宽度缩放到和按键一样大, 并同比例缩放高度
+//                double multi = 1.0 * iconW / key.width;
+//                int tempIconH = (int) (iconH / multi);
+//                if (tempIconH <= key.height) {
+//                    //宽度相等时, 图标的高度小于等于按键的高度, 按照现在的宽度和高度设置图标的最终宽度和高度
+//                    iconSizeHeight = tempIconH / ICON2KEY;
+//                    iconSizeWidth = key.width / ICON2KEY;
+//                } else {
+//                    //宽度相等时, 图标的高度大于按键的高度, 这时按键放不下图标, 需要重新按照高度缩放
+//                    double mul = 1.0 * iconH / key.height;
+//                    int tempIconW = (int) (iconW / mul);
+//                    iconSizeHeight = key.height / ICON2KEY;
+//                    iconSizeWidth = tempIconW / ICON2KEY;
+//                }
+//                setIconSize(canvas, key, iconSizeWidth, iconSizeHeight);
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 
     private void setIconSize(Canvas canvas, Keyboard.Key key, int iconSizeWidth, int iconSizeHeight) {
         int left = key.x + (key.width - iconSizeWidth) / 2;
